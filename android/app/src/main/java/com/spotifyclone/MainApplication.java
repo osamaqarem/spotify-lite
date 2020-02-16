46e88d9fa28d92e624d7f50c8cdb195c0186034c
Osama Qarem <osamaqarem@gmail.com>
2020-02-16 23:02:16 +0800

    New image color android native module

--- android/app/src/main/java/com/spotifyclone/MainApplication.java
+++ android/app/src/main/java/com/spotifyclone/MainApplication.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.spotifyclone.ColorHelper.ColorHelperPackage;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -24,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+           packages.add(new ColorHelperPackage());
           return packages;
         }
 
